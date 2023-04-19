75ab4100df5319a5236407d38fbf865ef28d629b
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-19 17:19:52 +0100

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,10 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Modules\Setting\app\Models\Setting;
 
 if (!function_exists('setting')) {
     function setting(String|array $name, $default = null)
     {
+        if (!Schema::hasTable('settings')) {
+            return $default;
+        }
+
         if (is_string($name)) {
             $setting = Setting::where('name', $name)->Orwhere('config', $name)->value('value') ?? $default;
             return $setting;
